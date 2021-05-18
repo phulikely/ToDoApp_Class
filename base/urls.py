d79e2a57108b45ac03803dcd8f39254fc3ebe01c
phulikely <phulikely@gmail.com>
2021-05-18 17:58:36 +0700

    class view on Django

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import RegisterPage, TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate, CustomLoginView
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='create-task'),
+    path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name='delete-task'),
+]
